ba07198b2cb25c3b143c983678542762c02c4a8d
Officialsmaj <salehmalaajimi@gmail.com>
2026-02-22 10:43:00 +0400

    up

--- script.js
+++ script.js
@@ -155,8 +155,8 @@ if (contactForm) {
         const body = encodeURIComponent(
             `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
         );
-
-        window.location.href = `mailto:officialsmaj@gmail.com?subject=${subject}&body=${body}`;
+        const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=officialsmaj@gmail.com&su=${subject}&body=${body}`;
+        window.location.href = gmailUrl;
     });
 }
  
